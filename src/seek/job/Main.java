4239af189b913f9199dd386f976b494a97095ee6
LikoGuan <likoguan@gmail.com>
2019-02-05 12:36:29 +1300

    init

---
+++ src/seek/job/Main.java
@@ -0,0 +1,91 @@
+package seek.job;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.jruby.RubyProcess;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            HBaseConfiguration conf = new HBaseConfiguration();
+            conf.addResource("hbase-site.xml");
+
+            HTable table = new HTable(conf, "test");
+            Get g = new Get("20170913-wechat-7233967608182".getBytes());
+            Result rs = table.get(g);
+            for (KeyValue kv : rs.raw()) {
+                System.out.print(new String(kv.getRow()) + "  ");
+                System.out.print(new String(kv.getFamily()) + ":");
+                System.out.print(new String(kv.getQualifier()) + "  ");
+                System.out.print(kv.getTimestamp() + "  ");
+                System.out.println(new String(kv.getValue()));
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+/*
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+//            StringTokenizer itr = new StringTokenizer(value.toString());
+//            while (itr.hasMoreTokens()) {
+//                word.set(itr.nextToken());
+//                context.write(word, one);
+//            }
+            String[] fields = value.toString().split(",");
+            context.write(new Text(fields[1]), new IntWritable(Integer.valueOf(fields[2])));
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(Main.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+    */
+}
